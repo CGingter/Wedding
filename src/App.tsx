5e2698150cf6a7bd60ddca5307b56b1bb3cf7f25
CGingter <christoph.gingter@rwth-aachen.de>
2025-07-30 11:19:43 +0200

    inital

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ minHeight: '100vh', display: 'grid', placeItems: 'center', background: '#f5f5f5' }}>
+      <h1 style={{ fontSize: '2rem', color: '#cc3366' }}>
+        Hallo Hochzeit! 🎉
+      </h1>
+    </div>
+  );
+}
